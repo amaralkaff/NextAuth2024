55b94ef9ea62edc6aabd379c7c88d8435fb9e1ad
Admin <tpamangly@gmail.com>
2024-03-08 10:54:15 -0400

    adding button

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+const AuthLyaout = ({ children }: any) => {
+    return (
+        <div>
+            <nav className="bg-red-500 text-white">
+                This is auth navbar
+            </nav>
+            {children}
+        </div>
+    )
+}
+
+export default AuthLyaout
